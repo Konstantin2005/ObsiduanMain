49412899f98562227ca66cd40f7b0b2eed367eb1
Konstantin <Kiselevk1001@gmail.com>
2026-06-10 17:04:01 +1000

    2026-06-10

--- ZettleKastem/.obsidian/plugins/live-graph/main.js
+++ ZettleKastem/.obsidian/plugins/live-graph/main.js
@@ -1,3 +1,3 @@
-module.exports = require("C:/obsidian/Main/Scripts/ObsidianPlugins/live-graph/live-graph-core.js")(
+module.exports = require("C:/obsidian/Main/Scripts/ObsidianPlugins/live-graph/live-graph-core-v2.js")(
   require("obsidian"),
 );
