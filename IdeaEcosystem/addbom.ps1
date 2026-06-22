11402cf025af7ebdc39ce2d8c8c1402d586496f1
Konstantin <Kiselevk1001@gmail.com>
2026-06-22 13:24:24 +1000

    Auto-commit: 2026-06-22 13:24:24

--- IdeaEcosystem/addbom.ps1
+++
@@ -1,4 +0,0 @@
-$source = Get-Content "C:\obsidian\Main\IdeaEcosystem\generate.ps1" -Raw -Encoding UTF8
-$utf8Bom = New-Object System.Text.UTF8Encoding($true)
-[System.IO.File]::WriteAllText("C:\obsidian\Main\IdeaEcosystem\generate.ps1", $source, $utf8Bom)
-Write-Host "BOM added successfully"
--- IdeaEcosystem/debug.ps1
+++
@@ -1,8 +0,0 @@
-$s = [System.IO.File]::ReadAllText('C:\obsidian\Main\IdeaEcosystem\generate.ps1')
-$idx = $s.IndexOf('ci.Aggregate')
-if ($idx -ge 0) {
-    Write-Host "Found ci.Aggregate at index: $idx"
-    Write-Host "Context: $($s.Substring($idx-10, 40))"
-} else {
-    Write-Host "ci.Aggregate NOT found"
-}
