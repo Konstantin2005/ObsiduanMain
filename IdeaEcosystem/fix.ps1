de1f71d99a69182e639989a0414b8e19936ad461
Konstantin <Kiselevk1001@gmail.com>
2026-06-22 13:23:32 +1000

    Auto-commit: 2026-06-22 13:23:32

--- IdeaEcosystem/fix.ps1
+++ IdeaEcosystem/fix.ps1
@@ -1,7 +1,6 @@
 $s = [System.IO.File]::ReadAllText('C:\obsidian\Main\IdeaEcosystem\generate.ps1')
-# Fix the template variable syntax for the Against field in here-string
-$old = '${' + '$ci.Aggregate}'
-$new = '$(' + '$ci.Aggregate)'
+$old = '$ci.Aggregate'
+$new = '$ci.Aggregate'
 $s = $s.Replace($old, $new)
 [System.IO.File]::WriteAllText('C:\obsidian\Main\IdeaEcosystem\generate.ps1', $s)
-Write-Host "Fixed template variable syntax"
+Write-Host "Fixed"
